c58aeabed45c19643d7352c8ee29015f69f20341
ahernandez <albertohernandez_91@yahoo.com>
2021-02-01 17:19:03 -0600

    form input authentication added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+const form = document.querySelector('#search__form')
+const input = document.querySelector('#form__input')
+const error = document.querySelector('.form__error')
+
+function showError(event){
+  event.preventDefault();
+  input.classList.add('input--error');
+  error.classList.add('show');
+}
+
+function handleForm(event){
+  if(input.value != ""){
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.value)){
+      return true;
+    } else {
+      showError(event);
+    }
+  } else {
+    showError(event);
+  }
+}
+
+
+form.addEventListener('submit', handleForm);
